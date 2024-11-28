166c73fe701154b8542646b8285fee076968b148
arthurcorrea <arthurcorrea321321@gmail.com>
2024-11-28 18:41:42 -0300

    feat: estilo da tabela da home

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -19,11 +19,11 @@
     <div class="p-12">
         <div class="max-w-7xl mx-auto sm:px-6 md:p-6 lg:px-8">
 
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <table class="table-auto w-full border-collapse border border-gray-300">
+            <div class="p-6 bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <table class=" table-auto w-full border-collapse border border-gray-300 ">
                     <thead>
-                        <tr class="bg-gray-700">
-                            <th class="border border-gray-800 p-2 dark:text-gray-200">Título</th>
+                        <tr class="bg-gray-700 ">
+                            <th class="border border-gray-300 p-2 dark:text-gray-200">Título</th>
                             <th class="border border-gray-300 p-2 dark:text-gray-200">Descrição</th>
                             <th class="border border-gray-300 p-2 dark:text-gray-200">Data</th>
                             <th class="border border-gray-300 p-2 dark:text-gray-200">Hora</th>
@@ -32,10 +32,10 @@
                     <tbody class="d-flex">
                         @foreach($tasks as $task)
                             <tr>
-                                <td class="border border-gray-300 p-2 dark:text-gray-200">{{ $task->title }}</td>
-                                <td class="border border-gray-300 p-2 dark:text-gray-200">{{ $task->description }}</td>
-                                <td class="border border-gray-300 p-2 dark:text-gray-200">{{ $task->date }}</td>
-                                <td class="border border-gray-300 p-2 dark:text-gray-200">{{ $task->time }}</td>
+                                <td class="border border-gray-300 p-2 dark:text-gray-200 text-center">{{ $task->title }}</td>
+                                <td class="border border-gray-300 p-2 dark:text-gray-200 text-center">{{ $task->description }}</td>
+                                <td class="border border-gray-300 p-2 dark:text-gray-200 text-center">{{ $task->date }}</td>
+                                <td class="border border-gray-300 p-2 dark:text-gray-200 text-center">{{ $task->time }}</td>
                             </tr>
                         @endforeach
                     </tbody>
